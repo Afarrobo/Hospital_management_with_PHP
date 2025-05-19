dc1e4013a0e6352f373094318426fdac68d76160
Afarrobo <afsinsultana2001@gmail.com>
2025-05-20 04:52:34 +0600

    last 1

--- include/adminlogin.php
+++ include/adminlogin.php
@@ -7,18 +7,15 @@ if (isset($_POST['login'])) {
     $password = $_POST['pass'];
     $error = array();
     if (empty($username)) {
-
         $error['admin'] = "Enter Username";
     } else if (empty($password)) {
         $error['admin'] = "Enter Password";
     }
     if (count($error) == 0) {
         $query = "SELECT * FROM admin WHERE username = '$username' AND password = '$password'";
-
         $result = mysqli_query($connect, $query);
         if (mysqli_num_rows($result) == 1) {
             echo "<script>alert('You have login as a admin')</script>";
-
             $_SESSION['admin'] = $username;
             header("location: admin/index.php");
             exit();
@@ -27,9 +24,7 @@ if (isset($_POST['login'])) {
         }
     }
 }
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -38,14 +33,46 @@ if (isset($_POST['login'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin log in</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body {
+            background-image: url(admin_bg.jpg);
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+
+        .jumbotron {
+            background: rgba(255, 255, 255, 0.95);
+            border-radius: 20px;
+            padding: 40px;
+            box-shadow: 0 8px 16px rgba(0, 0, 0, 0.3);
+        }
+
+        .form-control:focus {
+            border-color: #28a745;
+            box-shadow: 0 0 5px rgba(40, 167, 69, 0.5);
+        }
+
+        .btn-success:hover {
+            background-color: #218838;
+            border-color: #1e7e34;
+        }
+
+        img {
+            display: block;
+            margin: 0 auto 20px;
+        }
+
+        .alert-danger {
+            text-align: center;
+            font-weight: bold;
+        }
+    </style>
 </head>
 
-<body style="background-image: url(admin_bg.jpg);
- background-repeat:no-repeat; background-size:cover;">
-    <?php
-    include("header.php");
-    ?>
-    <div style="margin-top:20px ;"></div>
+<body>
+    <?php include("header.php"); ?>
+    <div style="margin-top:20px;"></div>
     <div class="container">
         <div class="col-md-12">
             <div class="row">
@@ -53,11 +80,9 @@ if (isset($_POST['login'])) {
                 <div class="col-md-6 jumbotron">
                     <img src="admin_form_pic.avif" class="img-fluid" style="max-width: 300px;">
                     <form method="post" class="my-2">
-
                         <div class="alert alert-danger">
                             <?php
                             if (isset($error['admin'])) {
-
                                 $show = $error['admin'];
                                 echo $show;
                             } else {
@@ -65,30 +90,25 @@ if (isset($_POST['login'])) {
                             }
                             echo $show;
                             ?>
-
                         </div>
 
-
                         <div class="form-group">
-                            <lable style="color: black;">Username</lable>
-                            <input type="text" name="uname" class="form-control"
-                                autocomplete="off" palceholder="Enter Username">
-
+                            <label style="color: black;">Username</label>
+                            <input type="text" name="uname" class="form-control" autocomplete="off" placeholder="Enter Username">
                         </div>
 
                         <div class="form-group">
                             <label style="color: black;">Password</label>
                             <input type="password" name="pass" class="form-control">
-
                         </div>
-                        <input type=submit name="login" class="btn btn-success" value="login">
+
+                        <input type="submit" name="login" class="btn btn-success btn-block" value="Login">
                     </form>
                 </div>
                 <div class="col-md-3"></div>
             </div>
         </div>
     </div>
-
 </body>
 
 </html>
--- include/patient_log.php
+++ include/patient_log.php
@@ -25,7 +25,6 @@ if (isset($_POST['login'])) {
 }
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -33,46 +32,81 @@ if (isset($_POST['login'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Patient_log_in</title>
+
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <style>
+        body {
+            background-color: rgba(108, 112, 243, 0.8);
+        }
+
+        .login-form {
+            background: #fff;
+            padding: 30px;
+            border-radius: 12px;
+            box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.2);
+            transition: 0.3s ease-in-out;
+        }
+
+        .login-form:hover {
+            transform: scale(1.02);
+        }
+
+        .form-control:focus {
+            box-shadow: none;
+            border-color: #6c70f3;
+        }
+
+        .btn-info:hover {
+            background-color: #5a5ee0;
+        }
+
+        a:hover {
+            text-decoration: none;
+            font-weight: bold;
+        }
+    </style>
 </head>
 
-<body style="   background-color: rgba(108, 112, 243, 0.8) ;">
+<body style="background-color:rgb(115, 130, 188);">
 
+    <?php include("header.php"); ?>
 
-    <?php
-    include("header.php");
-    ?>
-
-    <div class="container-fluid">
+    <div class=" container-fluid">
         <div class="col-md-12">
-            <div class="row">
-                <div class="col-md-3"></div>
-                <div class="col-md-6 my-5 jumbotron">
-                    <h5 class="text-center my-3">Patient Login</h5>
-                    <form method="post">
-                        <div class="form-group">
-                            <label>Username</label>
-                            <input type="text" name="uname" class="form-control" autocomplete="off" placeholder="Enter Username">
-                        </div>
+            <div class="row justify-content-center">
+                <div class="col-md-6 my-5">
+                    <div class="login-form">
+                        <h4 class="text-center mb-4 text-primary"><i class="fas fa-user-circle"></i> Patient Login</h4>
+                        <form method="post">
+                            <div class="form-group">
+                                <label><i class="fas fa-user"></i> Username</label>
+                                <input type="text" name="uname" class="form-control" autocomplete="off" placeholder="Enter Username">
+                            </div>
 
-                        <div class="form-group">
-                            <label>Password</label>
-                            <input type="password" name="pass" class="form-control" autocomplete="off" placeholder="Enter Password">
-                        </div>
+                            <div class="form-group">
+                                <label><i class="fas fa-lock"></i> Password</label>
+                                <input type="password" name="pass" class="form-control" autocomplete="off" placeholder="Enter Password">
+                            </div>
 
-                        <input type="submit" name="login" class="btn btn-info my-3" value="Login">
+                            <input type="submit" name="login" class="btn btn-info btn-block" value="Login">
 
-                        <p> I don't have an account<a href="acc_01.php">Click Here</a>
-                    </form>
-
-
+                            <p class="text-center mt-3">I don't have an account <a href="acc_01.php">Click Here</a></p>
+                        </form>
+                    </div>
                 </div>
-                <div class="col-md-3"></div>
             </div>
         </div>
     </div>
 
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
-
-
 
 </html>
